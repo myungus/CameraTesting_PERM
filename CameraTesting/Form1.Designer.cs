7ca7583f2dfcd6cfbb18e448b179e0f0dba34c3f
Miguel Pereira <miguel.pereira@nov.com>
2019-07-05 12:00:55 +0200

    Initial Push

---
+++ CameraTesting/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CameraTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.camera2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.camera3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(497, 275);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1547, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cameraToolStripMenuItem,
+            this.camera2ToolStripMenuItem,
+            this.camera3ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.pauseToolStripMenuItem});
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cameraToolStripMenuItem.Text = "Camera1";
+            this.cameraToolStripMenuItem.Click += new System.EventHandler(this.CameraToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.StartToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.StopToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.PauseToolStripMenuItem_Click);
+            // 
+            // camera2ToolStripMenuItem
+            // 
+            this.camera2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem1,
+            this.stopToolStripMenuItem1,
+            this.pauseToolStripMenuItem1});
+            this.camera2ToolStripMenuItem.Name = "camera2ToolStripMenuItem";
+            this.camera2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.camera2ToolStripMenuItem.Text = "Camera2";
+            // 
+            // startToolStripMenuItem1
+            // 
+            this.startToolStripMenuItem1.Name = "startToolStripMenuItem1";
+            this.startToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem1.Text = "Start";
+            this.startToolStripMenuItem1.Click += new System.EventHandler(this.StartToolStripMenuItem1_Click);
+            // 
+            // stopToolStripMenuItem1
+            // 
+            this.stopToolStripMenuItem1.Name = "stopToolStripMenuItem1";
+            this.stopToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.stopToolStripMenuItem1.Text = "Stop";
+            this.stopToolStripMenuItem1.Click += new System.EventHandler(this.StopToolStripMenuItem1_Click);
+            // 
+            // pauseToolStripMenuItem1
+            // 
+            this.pauseToolStripMenuItem1.Name = "pauseToolStripMenuItem1";
+            this.pauseToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.pauseToolStripMenuItem1.Text = "Pause";
+            this.pauseToolStripMenuItem1.Click += new System.EventHandler(this.PauseToolStripMenuItem1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(537, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(497, 275);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // camera3ToolStripMenuItem
+            // 
+            this.camera3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem2,
+            this.stopToolStripMenuItem2,
+            this.pauseToolStripMenuItem2});
+            this.camera3ToolStripMenuItem.Name = "camera3ToolStripMenuItem";
+            this.camera3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.camera3ToolStripMenuItem.Text = "Camera3";
+            // 
+            // startToolStripMenuItem2
+            // 
+            this.startToolStripMenuItem2.Name = "startToolStripMenuItem2";
+            this.startToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem2.Text = "Start";
+            this.startToolStripMenuItem2.Click += new System.EventHandler(this.StartToolStripMenuItem2_Click);
+            // 
+            // stopToolStripMenuItem2
+            // 
+            this.stopToolStripMenuItem2.Name = "stopToolStripMenuItem2";
+            this.stopToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.stopToolStripMenuItem2.Text = "Stop";
+            this.stopToolStripMenuItem2.Click += new System.EventHandler(this.StopToolStripMenuItem2_Click);
+            // 
+            // pauseToolStripMenuItem2
+            // 
+            this.pauseToolStripMenuItem2.Name = "pauseToolStripMenuItem2";
+            this.pauseToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.pauseToolStripMenuItem2.Text = "Pause";
+            this.pauseToolStripMenuItem2.Click += new System.EventHandler(this.PauseToolStripMenuItem2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1040, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(497, 275);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Left Camera";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(748, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Center Camera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1275, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Right Camera";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1547, 388);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem camera2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem camera3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CameraTesting/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CameraTesting
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture captureL;
+        VideoCapture captureC;
+        VideoCapture captureR;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (captureL==null)
+            {
+                captureL = new Emgu.CV.VideoCapture(2);
+            }
+            captureL.ImageGrabbed += Capture_ImageGrabbed1;
+            captureL.Start();
+        }
+
+        private void Capture_ImageGrabbed1(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                captureL.Retrieve(m);
+                pictureBox1.Image = m.ToImage<Bgr, byte>().Bitmap;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void StopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (captureL!=null)
+            {
+                captureL = null;
+            }
+        }
+
+        private void PauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (captureL!=null)
+            {
+                captureL.Pause();
+            }
+        }
+
+        private void CameraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (captureC == null)
+            {
+                captureC = new Emgu.CV.VideoCapture(3);
+            }
+            captureC.ImageGrabbed += Capture_ImageGrabbed2;
+            captureC.Start();
+       
+        }
+
+        private void Capture_ImageGrabbed2(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat n = new Mat();
+                captureC.Retrieve(n);
+                pictureBox2.Image = n.ToImage<Bgr, byte>().Bitmap;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void StopToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (captureC != null)
+            {
+                captureC = null;
+            }
+        }
+
+        private void PauseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (captureC != null)
+            {
+                captureC.Pause();
+            }
+        }
+
+        private void StartToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (captureR == null)
+            {
+                captureR = new Emgu.CV.VideoCapture(0);
+            }
+            captureR.ImageGrabbed += CaptureR_ImageGrabbed3;
+            captureR.Start();
+        }
+
+        private void CaptureR_ImageGrabbed3(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat o = new Mat();
+                captureR.Retrieve(o);
+                pictureBox3.Image = o.ToImage<Bgr, byte>().Bitmap;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void StopToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (captureR != null)
+            {
+                captureR = null;
+            }
+        }
+
+        private void PauseToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (captureR != null)
+            {
+                captureR.Pause();
+            }
+        }
+    }
+}
